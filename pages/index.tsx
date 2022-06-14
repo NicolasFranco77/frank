3e981472079020edf72e49fd995490ab002384a8
Nicolas <nicolasadrianfranco@gmail.com>
2022-06-14 18:55:56 -0300

    reinitialized files

---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from "next";
+
+//components
+import Navbar from "../src/components/Navbar/Navbar";
+
+import { Text } from "@chakra-ui/react";
+
+const Home: NextPage = () => {
+  return (
+   
+      <Navbar />
+  
+  );
+};
+
+export default Home;
